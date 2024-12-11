571406c915682310ce7321b6fbeced36f6b7ac43
BondarevTK <BondarevTK101@gmail.com>
2024-12-11 20:38:25 +0300

    Task1

---
+++ Tyuiu.BondarevTK.Sprint3.Task1.V11.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.BondarevTK.Sprint3.Task1.V11.Lib
+{
+    public class DataService : ISprint3Task1V11
+    {
+        public double GetMultiplySeries(double value, int startValue, int stopValue)
+        {
+            int i = startValue;
+            double sum = 0;
+            while (i < stopValue)
+            {
+                sum = sum + Math.Pow(value,i) + 1/2;
+            }
+        }
+    }
+}
