08bbae145ea175aaba2597ef42fe6a1532a3b3a7
BondarevTK <BondarevTK101@gmail.com>
2024-12-13 17:26:52 +0300

    Task6

---
+++ Tyuiu.BondarevTK.Sprint3.Task6.V11.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.BondarevTK.Sprint3.Task6.V11.Lib
+{
+    public class DataService : ISprint3Task6V11
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int count = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                if (i%10==0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.BondarevTK.Sprint3.Task6.V11/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.BondarevTK.Sprint3.Task6.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
