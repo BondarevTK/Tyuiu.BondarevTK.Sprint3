d54eef63dd55336c0c1aaa0143fa940a27d414a1
BondarevTK <BondarevTK101@gmail.com>
2024-12-14 08:42:24 +0300

    t7

--- Tyuiu.BondarevTK.Sprint3.Task7.V5.Test/DataServiceTest.cs
+++ Tyuiu.BondarevTK.Sprint3.Task7.V5.Test/DataServiceTest.cs
@@ -6,6 +6,11 @@ namespace Tyuiu.BondarevTK.Sprint3.Task7.V5.Test
         [TestMethod]
         public void TestMethod1()
         {
+            int startvalue = -5;
+            int stopvalue = 5;
+            int len = stopvalue - startvalue + 1;
+            double[] arraywait = new double[len];
+
         }
     }
 }
